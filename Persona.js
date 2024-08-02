b826ab3e96f6c31a3e16cc32dad800f937fd7c96
gero-trujillo <gtutos3@gmail.com>
2024-08-02 16:05:04 -0500

    Encapsulamiento - Implementacion de Clase Persona por Gero

---
+++ Persona.js
@@ -0,0 +1,10 @@
+class Persona {
+  #nombre = "";
+  #edad = "";
+  #fecNacimiento = "";
+  constructor(nombre, edad, fecNacimiento) {
+    this.#nombre = nombre;
+    this.#edad = edad;
+    this.#fecNacimiento = fecNacimiento;
+  }
+}
