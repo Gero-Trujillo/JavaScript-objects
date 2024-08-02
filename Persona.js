d37d3bbb0657c4f913385436d265853f6c6c37cf
gero-trujillo <gtutos3@gmail.com>
2024-08-02 16:07:21 -0500

    Encapsulamiento - Implementacion de Metodos para manipulacio de propiedades por Gero

--- Persona.js
+++ Persona.js
@@ -7,4 +7,25 @@ class Persona {
     this.#edad = edad;
     this.#fecNacimiento = fecNacimiento;
   }
+
+  establecerNombre(nombre){
+    this.#nombre = nombre;
+  }
+  obtenerNombre(){
+    return this.#nombre;
+  }
+
+  establecerEdad(edad){
+    this.#edad = edad;
+  }
+  obtenerEdad(){
+    return this.#edad;
+  }
+
+  establecerFecNacimiento(fecNacimiento){
+    this.#fecNacimiento = fecNacimiento;
+  }
+  obtenerFecNacimiento(){
+    return this.#fecNacimiento;
+  }
 }
