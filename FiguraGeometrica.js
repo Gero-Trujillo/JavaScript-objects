57e6dcd0ab309eec561743ca675cd63d70f93392
gero-trujillo <gtutos3@gmail.com>
2024-08-02 21:24:45 -0500

    Encapsulamiento y Herencia - Implementación de Métodos para Manipulación de Propiedades por Gero

--- FiguraGeometrica.js
+++ FiguraGeometrica.js
@@ -5,4 +5,40 @@ class FiguraGeometrica{
         this.#nombre = nombre;
         this.#color = color;
     }
+    establecerNombre(nombre){
+        if(typeof nombre !== "string"){
+            console.error("El nombre debe ser un texto");
+            return;
+        }
+        if(nombre === ""){
+            console.error("El nombre no puede estar vacío");
+            return;
+        }
+        this.#nombre = nombre;
+    }
+    obtenerNombre(){
+        if(this.#nombre === ""){
+            console.error("El nombre no puede estar vacío");
+            return;
+        }
+        return this.#nombre;
+    }
+    establecerColor(color){
+        if(typeof color !== "string"){
+            console.error("El color debe ser un texto");
+            return;
+        }
+        if(color === ""){
+            console.error("El color no puede estar vacío");
+            return;
+        }
+        this.#color = color;
+    }
+    obtenerColor(){
+        if(this.#color === ""){
+            console.error("El color no puede estar vacío");
+            return;
+        }
+        return this.#color;
+    }
 }
